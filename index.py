1562296d82916613f365dae272690dd7ef1e8a58
juliosanchez-jamcity <julio_sanchez04@yahoo.com>
2020-03-06 16:47:29 -0800

    renamed to marketingManager

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 #%%
-from dataManager import DataManager
+from MarketingManager import MarketingManager
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,32 +11,32 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 import scipy.interpolate 
 #%%
-dm = DataManager()
+dm = MarketingManager()
 th = th.theseus()
 
 #%%
-ad_rev = dm.ironsourceDataFrame('2019-12-01','2019-12-31')
+# ad_rev = dm.ironsourceDataFrame('2019-12-01','2019-12-31')
 #%%
-ad_rev['date'] = pd.to_datetime(ad_rev['date'])
-ad_rev.set_index('date', inplace=True)
-panda = ad_rev[ad_rev['appName'].str.contains('Panda')]
-pp_rev = panda['revenue'].resample('d').sum()
-pp_rev = pp_rev.reset_index()
-pp_rev = pp_rev.drop('date', axis = 1)
-pp_rev.head()
-#%%
-dau = dm.dailyActiveUserDataframe('dau_data')
-dau.head()
+# ad_rev['date'] = pd.to_datetime(ad_rev['date'])
+# ad_rev.set_index('date', inplace=True)
+# panda = ad_rev[ad_rev['appName'].str.contains('Panda')]
+# pp_rev = panda['revenue'].resample('d').sum()
+# pp_rev = pp_rev.reset_index()
+# pp_rev = pp_rev.drop('date', axis = 1)
+# pp_rev.head()
+# #%%
+# dau = dm.dailyActiveUserDataframe('dau_data')
+# dau.head()
 
-#%%
-pp_rev['arpdau'] = pp_rev['revenue'] / dau['DAU']
-#%%
-pp_rev.head()
-#%%
-pp_rev.to_csv('adrev.csv')
+# #%%
+# pp_rev['arpdau'] = pp_rev['revenue'] / dau['DAU']
+# #%%
+# pp_rev.head()
+# #%%
+# pp_rev.to_csv('adrev.csv')
 #%%
 reten = dm.retentionDataFrame('retention_data')
-reten.set_index('INSTALL_DT', inplace = True)
+reten.set_index('install_dt', inplace = True)
 reten.index = pd.to_datetime(reten.index)
 reten = dm.calculateRetentionPct(reten)
 reten.head()
@@ -52,85 +52,6 @@ y_data = dm.totalRetentionAverage(reten).tolist()
 retention_profile = th.create_profile(x_days, y_data[:-1])
 th.plot_retention(retention_profile)
 #%%
-retention_projection = retention_profile['retention_projection'][1]
-profile = pd.DataFrame(retention_projection)
-profile.to_csv('retention_pro.csv')
-# %%
-print(len(retention_projection))
+retention = retention_profile['retention_projection'][1]
+days = pd.DataFrame(np.arange(0,60))
 
-# %%
-days = np.arange(0,366)
-retention_days = days[:60]
-re_days = pd.DataFrame(retention_days)
-#%%
-reg = LinearRegression()
-reg.fit(re_days, profile)
-#%%
-print(reg.score(re_days, retention_projection))
-
-#%%
-reg.predict(profile)
-#%%
-print(reg.predict(profile[-1]))
-print(profile)
-# %%
-retention_days = days[:60]
-
-log_days = np.log(retention_days)
-log_retention = np.log(retention_projection)
-area = log_retention/log_days
-a = area[-1]
-
-print(a)
-#%%
-user_lifetime = []
-for index in days:
-    agg = (1/(a - 1) * (1 - index ** (1-a)))
-    user_lifetime.append(agg)
-
-#%%
-print(len(log_retention))
-
-#%%
-# arpdu = float(monthly['AD_REVENUE'] / monthly['INSTALL_QTY'])
-arpdu = pp_rev['arpdau'].mean()
-print(arpdu)
-# %%
-ltv = []
-for item in user_lifetime:
-    lagg = item * arpdu
-    ltv.append(lagg)
-
-#%%
-print(ltv)
-
-# %%
-plt.plot(days, ltv)
-plt.show()
-
-# %%
-LTV_df = pd.DataFrame({
-    'days': days,
-    'user_lifetime': user_lifetime,
-    'LTV': ltv,
-})
-LTV_df.to_csv('panda_ads_ltv_dec.csv')
-
-
-# %%
-average_rev = reten['AD_REVENUE'] / reten['INSTALL_QTY']
-
-# %%
-average_rev.head()
-
-
-
-# %%
-average_rev.to_csv('ad_rev_per_install.csv')
-
-# %%
-temp = dm.LTV_Model_1(retention_days,retention_projection, arpdu)
-temp.head()
-temp.to_csv('all_data.csv')
-
-# %%
